31cc6409285f1f45e196d62022b88b3f0bf2663c
Dennis Lujan <124803452+Haimdall14@users.noreply.github.com>
2023-04-14 10:10:24 -0500

    Add files via upload

---
+++ codetemp.rb
@@ -0,0 +1,28 @@
+
+
+
+def sum(name, *numbers)
+# name = Lobelto
+# *numbers = [1, 2, 3, 4, 5, 6, 7, 8] ARRAY
+  result = 0
+  numbers.each { |number| result += number }
+
+  "hello #{name}, the result is => #{result}"
+end
+
+p sum("Lobelto", 1, 2, 3, 4, 5, 6, 7, 8)
+
+# Se imprime:
+# "Hello Lobelto, the result is => 36"
+
+
+
+
+
+
+
+
+
+
+
+
